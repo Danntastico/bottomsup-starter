a021f0fe1e2de5d2a79c15905c4668becf9155bf
Danilo Peña <nicolasdpf@gmail.com>
2020-04-13 02:34:21 -0500

    Adding function to check internet connection

--- app/src/main/java/com/raywenderlich/android/bottomsup/ui/feed/BeersActivity.kt
+++ app/src/main/java/com/raywenderlich/android/bottomsup/ui/feed/BeersActivity.kt
@@ -24,7 +24,12 @@ package com.raywenderlich.android.bottomsup.ui.feed
 
 import android.content.Context
 import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.net.NetworkInfo
+import android.net.wifi.p2p.WifiP2pManager
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.DefaultItemAnimator
@@ -43,9 +48,6 @@ class BeersActivity : AppCompatActivity() {
   private val viewModel by lazy { getViewModel<BeersViewModel>()}
   private val adapter = BeersAdapter()
 
-  val Context.isConnected : Boolean get() {
-    return (getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager).activeNetworkInfo?.isConnected == true
-  }
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
@@ -74,6 +76,37 @@ class BeersActivity : AppCompatActivity() {
   private fun setErrorVisibility(shouldShow: Boolean){
     errorView.visibility = if (shouldShow) View.VISIBLE else View.GONE
     beersList.visibility = if (!shouldShow) View.VISIBLE else View.GONE
+  }
+
+  private fun isInternetAvailable(context: Context): Boolean {
+    var result = false
+    val connectivityManager =
+            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+      val networkCapabilities = connectivityManager.activeNetwork ?: return false
+      val actNw =
+              connectivityManager.getNetworkCapabilities(networkCapabilities) ?: return false
+      result = when {
+        actNw.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+        actNw.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+        actNw.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
+        else -> false
+      }
+    } else {
+      connectivityManager.run {
+        connectivityManager.activeNetworkInfo?.run {
+          result = when (type) {
+            ConnectivityManager.TYPE_WIFI -> true
+            ConnectivityManager.TYPE_MOBILE -> true
+            ConnectivityManager.TYPE_ETHERNET -> true
+            else -> false
+          }
+
+        }
+      }
+    }
+
+    return result
   }
   //acá puede ir mi método para la carga infinita!
 }
