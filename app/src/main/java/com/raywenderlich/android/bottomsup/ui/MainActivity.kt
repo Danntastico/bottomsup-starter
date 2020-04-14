55382fe83c97d9dcdbba1af9ab12eb5c5ab97bbe
Danilo Peña <nicolasdpf@gmail.com>
2020-04-14 18:30:11 -0500

    Ready to change

--- app/src/main/java/com/raywenderlich/android/bottomsup/ui/MainActivity.kt
+++ app/src/main/java/com/raywenderlich/android/bottomsup/ui/MainActivity.kt
@@ -2,6 +2,7 @@ package com.raywenderlich.android.bottomsup.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.raywenderlich.android.bottomsup.R
 
 class MainActivity : AppCompatActivity() {
 
