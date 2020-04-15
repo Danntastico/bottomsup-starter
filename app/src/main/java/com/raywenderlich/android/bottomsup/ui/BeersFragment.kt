e490344f8d1dba08799dce92e8c6de2743ed966b
Danilo Peña <nicolasdpf@gmail.com>
2020-04-14 19:01:33 -0500

    Migrating to BeersFragment
    Migrating my BeersActivity methods to beers fragment

--- app/src/main/java/com/raywenderlich/android/bottomsup/ui/BeersFragment.kt
+++ app/src/main/java/com/raywenderlich/android/bottomsup/ui/BeersFragment.kt
@@ -1,24 +1,30 @@
 package com.raywenderlich.android.bottomsup.ui
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.DefaultItemAnimator
+import androidx.recyclerview.widget.GridLayoutManager
 
 import com.raywenderlich.android.bottomsup.R
-import com.raywenderlich.android.bottomsup.common.getViewModel
+import com.raywenderlich.android.bottomsup.common.subscribe
 import com.raywenderlich.android.bottomsup.ui.feed.adapter.BeersAdapter
 import com.raywenderlich.android.bottomsup.viewmodel.BeersViewModel
 import com.raywenderlich.android.bottomsup.viewmodel.ConnViewModel
+import kotlinx.android.synthetic.main.activity_beers.*
+import kotlinx.android.synthetic.main.fragment_beers.*
 
 /**
  * A simple [Fragment] subclass.
  */
 class BeersFragment : Fragment() {
 
-    private val viewModel by lazy { getViewModel<BeersViewModel>()}
-    private val connViewModel by lazy {getViewModel<ConnViewModel>()}
+    private lateinit var viewModel: BeersViewModel
+    private lateinit var connViewModel: ConnViewModel
     private val adapter = BeersAdapter()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
@@ -27,4 +33,43 @@ class BeersFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_beers, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initializeUi(view)
+        viewModel.errorData.subscribe(this, this::setErrorVisibility)
+        viewModel.loadingData.subscribe(this, this::showLoading)
+        viewModel.pageData.subscribe(this, adapter::clearIfNeeded)
+        viewModel.beerData.subscribe(this, adapter::addItems)
+
+        connViewModel.connectivity.observe(this, Observer {
+            it?.run {
+                if(it) {
+                    Log.d("heeeeeeyyyyyyyy","INTERNET ON")
+                    ConnAlert2.visibility = View.GONE
+                    initializeUi(view)
+                    viewModel.getBeers()
+                } else {
+                    Log.d("heeeeeeyyyyyyyy", "INTERNET OFF")
+                    ConnAlert2.visibility = View.VISIBLE
+                }
+            }
+        })
+        viewModel.getBeers()
+
+    }
+
+    private fun initializeUi(view: View){
+        beersListF.layoutManager = GridLayoutManager(view.context, 2)
+        beersListF.itemAnimator = DefaultItemAnimator()
+        beersListF.adapter = adapter
+
+        pullToRefreshF.setOnRefreshListener(viewModel::onRefresh)
+    }
+    private fun showLoading(isLoading: Boolean){
+        pullToRefreshF.isRefreshing = isLoading
+    }
+    private fun setErrorVisibility(shouldShow: Boolean){
+        errorViewF.visibility = if (shouldShow) View.VISIBLE else View.GONE
+        beersListF.visibility = if (!shouldShow) View.VISIBLE else View.GONE
+    }
 }
