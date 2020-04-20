798eca00eb753b65172a840f829db1ab0d6d40f5
Danilo Peña <nicolasdpf@gmail.com>
2020-04-20 01:09:25 -0500

    Adding ViewModel to my BeersDataSource

--- app/src/main/java/com/raywenderlich/android/bottomsup/ui/BeersDataSource.kt
+++ app/src/main/java/com/raywenderlich/android/bottomsup/ui/BeersDataSource.kt
@@ -2,17 +2,21 @@ package com.raywenderlich.android.bottomsup.ui
 
 import androidx.paging.PageKeyedDataSource
 import com.raywenderlich.android.bottomsup.model.Beer
+import com.raywenderlich.android.bottomsup.viewmodel.BeersViewModel
 
 class BeersDataSource: PageKeyedDataSource<String, Beer>(){
+
+    private lateinit var viewModel : BeersViewModel
+
     override fun loadInitial(params: LoadInitialParams<String>, callback: LoadInitialCallback<String, Beer>) {
-        TODO("Not yet implemented")
+        viewModel.getBeers()
     }
 
     override fun loadAfter(params: LoadParams<String>, callback: LoadCallback<String, Beer>) {
-        TODO("Not yet implemented")
+        viewModel.getBeers()
     }
 
     override fun loadBefore(params: LoadParams<String>, callback: LoadCallback<String, Beer>) {
-        TODO("Not yet implemented")
+        viewModel.getBeers()
     }
 }
