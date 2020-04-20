bbcbedea0fac76dd8659aae1765ebff010666008
Danilo Peña <nicolasdpf@gmail.com>
2020-04-19 23:15:42 -0500

    Implementing methods for PageKeyedDatasource

--- app/src/main/java/com/raywenderlich/android/bottomsup/ui/BeersDataSource.kt
+++ app/src/main/java/com/raywenderlich/android/bottomsup/ui/BeersDataSource.kt
@@ -1,4 +1,18 @@
 package com.raywenderlich.android.bottomsup.ui
 
-class BeersDataSource {
+import androidx.paging.PageKeyedDataSource
+import com.raywenderlich.android.bottomsup.model.Beer
+
+class BeersDataSource: PageKeyedDataSource<String, Beer>(){
+    override fun loadInitial(params: LoadInitialParams<String>, callback: LoadInitialCallback<String, Beer>) {
+        TODO("Not yet implemented")
+    }
+
+    override fun loadAfter(params: LoadParams<String>, callback: LoadCallback<String, Beer>) {
+        TODO("Not yet implemented")
+    }
+
+    override fun loadBefore(params: LoadParams<String>, callback: LoadCallback<String, Beer>) {
+        TODO("Not yet implemented")
+    }
 }
